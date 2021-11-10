5755e768d43012d39eec31ef13db885558a6d945
priyank-pathak <88475213+priyank-pathak@users.noreply.github.com>
2021-11-10 11:48:10 +0530

    Image Slider java script project

--- script.js
+++ script.js
@@ -1,27 +1,141 @@
-let screen = document.getElementById("screen");
-buttons = document.querySelectorAll("button");
-let screenValue = "";
-for (item of buttons) {
-    item.addEventListener('click',(e)=>{
-        buttonText  = e.target.innerText;
-        console.log("Button text is",buttonText);
-        if(buttonText == '*'){
-           
-           screenValue += buttonText;
+let i = 0;
 
-           screen.value = screenValue;
-        }
-        else if (buttonText == 'C') {
-            screenValue = "";
-            screen.value = screenValue;
-        
-        }
-        else if(buttonText == "="){
-            screen.value = eval(screenValue);
-        }
-        else{
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
-    })
+
+let box1 = document.getElementById("image1");
+
+let box2 = document.getElementById("image2");
+
+let box3 = document.getElementById("image3");
+
+let box4 = document.getElementById("image4");
+
+
+let a1 = document.querySelector(".a1");
+
+let a2 = document.querySelector(".a2");
+
+let a3 = document.querySelector(".a3");
+
+let a4 = document.querySelector(".a4");
+
+
+a1.onclick = function () {
+    box1.style.opacity = 1;
+    box2.style.opacity = 0;
+    box3.style.opacity = 0;
+    box4.style.opacity = 0;
+    info.innerHTML = "Image 1";
+    i = 0;
+}
+
+a2.onclick = function () {
+    box1.style.opacity = 0;
+    box2.style.opacity = 1;
+    box3.style.opacity = 0;
+    box4.style.opacity = 0;
+    info.innerHTML = "Image 2";
+    i = 1;
+}
+
+a3.onclick = function () {
+    box1.style.opacity = 0;
+    box2.style.opacity = 0;
+    box3.style.opacity = 1;
+    box4.style.opacity = 0;
+    info.innerHTML = "Image 3";
+    i = 2;
+}
+
+a4.onclick = function () {
+    box1.style.opacity = 0;
+    box2.style.opacity = 0;
+    box3.style.opacity = 0;
+    box4.style.opacity = 1;
+    info.innerHTML = "Image 4";
+    i = 3;
+}
+document.addEventListener("keydown",(e)=>{
+    if (e.keycode==37) {
+    left();
+    }
+});
+
+
+document.addEventListener("keydown",(e)=>{
+    if (e.keycode==39) {
+    right();
+    }
+});
+
+function right() {
+    if(i==0){
+    box1.style.opacity = 1;
+    box2.style.opacity = 0;
+    box3.style.opacity = 0;
+    box4.style.opacity = 0;
+    info.innerHTML = "Image 1"
+    i++;
+
+}
+else if (i == 1){
+    box1.style.opacity = 0;
+    box2.style.opacity = 1;
+    box3.style.opacity = 0;
+    box4.style.opacity = 0;
+    info.innerHTML = "Image 2"
+    i++;
+}
+
+else if (i == 2){
+    box1.style.opacity = 0;
+    box2.style.opacity = 0;
+    box3.style.opacity = 1;
+    box4.style.opacity = 0;
+    info.innerHTML = "Image 3"
+    i++;
+}
+else if (i == 3){
+    box1.style.opacity = 0;
+    box2.style.opacity = 0;
+    box3.style.opacity = 0;
+    box4.style.opacity = 1;
+    info.innerHTML = "Image 4"
+    i = 0 ;
+}
+}
+
+function left() {
+    if (i == 0) {
+    box1.style.opacity = 0;
+    box2.style.opacity = 0;
+    box3.style.opacity = 0;
+    box4.style.opacity = 1;
+    info.innerHTML = "Image 1"
+    i++;
+    }
+    else if (i == 1){
+        box1.style.opacity = 0;
+        box2.style.opacity = 0;
+        box3.style.opacity = 1;
+        box4.style.opacity = 0;
+        info.innerHTML = "Image 2"
+        i++;
+    }
+    else if (i == 2){
+        box1.style.opacity = 0;
+        box2.style.opacity = 1;
+        box3.style.opacity = 0;
+        box4.style.opacity = 0;
+        info.innerHTML = "Image 3"
+        i++;
+    }
+    else if (i == 3){
+        box1.style.opacity = 1;
+        box2.style.opacity = 0;
+        box3.style.opacity = 0;
+        box4.style.opacity = 0;
+        info.innerHTML = "Image 4"
+        i = 0;
+    }
+
 }
