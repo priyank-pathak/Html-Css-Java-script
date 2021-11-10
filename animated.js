f9c97fa4e1ca318c002ae4017659ddc3a4e71587
priyank-pathak <88475213+priyank-pathak@users.noreply.github.com>
2021-11-10 11:41:10 +0530

    login form with js

---
+++ animated.js
@@ -0,0 +1,9 @@
+
+function clickFunction() {
+  alert('U Are Now in best website.');
+}
+
+function dblFunction() {
+  confirm('Do You Wanna Really Get out ?');
+  prompt("Enter Your Full Name: ");
+}
